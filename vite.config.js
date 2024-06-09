b75e13ae637384de7d382e6f5b0791ef0eda65f3
MANOHARGARIGANTI29 <garigantimanohar29@gmail.com>
2024-06-09 05:47:13 +0530

    Front-end

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
-// https://vitejs.dev/config/
+// Load environment variables from .env file
+dotenv.config()
+
 export default defineConfig({
-  base:"/VerveBridge-Note-Keeping-App/",
+  base: "/VerveBridge-Note-Keeping-App/",
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000 // Use the PORT environment variable or default to 3000
+  }
 })
